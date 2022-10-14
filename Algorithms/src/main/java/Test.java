bf88fd69f0c9d1813407e9116f351d48cf7312ea
Kochuev.Dmitry <kochuevdmitry@googlemail.com>
2022-10-14 20:24:38 +0300

    Social Network loaded

---
+++ Algorithms/src/main/java/Test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Test {
+
+    public void test(){
+
+        Set<String> stringSet = new LinkedHashSet<>();
+        List<String> stringList = new ArrayList<>();
+
+    }
+}
